292a439c1b91b23ea84b640a17d3d309d4d12431
Eduardo Arruda <13435770+eduardo-arruda@users.noreply.github.com>
2025-07-02 21:38:04 -0300

    Ajusta postgresql e sonarqube

--- backend/utils/postgresql/databases.sql
+++ backend/utils/postgresql/databases.sql
@@ -1,26 +1,44 @@
--- Criação de usuários para cada serviço
-CREATE USER sonar WITH PASSWORD 'a12345678';
-CREATE USER classes WITH PASSWORD 'a12345678';
-CREATE USER courses WITH PASSWORD 'a12345678';
-CREATE USER lessons WITH PASSWORD 'a12345678';
-CREATE USER professors WITH PASSWORD 'a12345678';
-CREATE USER reservations WITH PASSWORD 'a12345678';
-CREATE USER resources WITH PASSWORD 'a12345678';
-CREATE USER rooms WITH PASSWORD 'a12345678';
-CREATE USER students WITH PASSWORD 'a12345678';
+-- Script de inicialização dos bancos de dados para o projeto ConstrSW
+-- Este script deve ser executado como usuário postgres
 
--- Criação dos bancos de dados
-CREATE DATABASE sonar TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
-CREATE DATABASE classes TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
-CREATE DATABASE courses TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
-CREATE DATABASE lessons TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
-CREATE DATABASE professors TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
-CREATE DATABASE reservations TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
-CREATE DATABASE resources TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
-CREATE DATABASE rooms TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
-CREATE DATABASE students TEMPLATE template0 ENCODING 'LATIN1' LC_COLLATE 'C' LC_CTYPE 'C' TABLESPACE pg_default CONNECTION LIMIT -1;
+-- =====================================================
+-- FASE 1: Conectar como postgres e criar usuários
+-- =====================================================
 
--- Conceder privilégios aos usuários em seus respectivos bancos
+-- Conectar como postgres (usuário padrão)
+\c postgres;
+
+-- Criação de usuários para cada serviço com permissão CREATEDB
+CREATE USER sonar WITH PASSWORD 'a12345678' CREATEDB;
+CREATE USER classes WITH PASSWORD 'a12345678' CREATEDB;
+CREATE USER courses WITH PASSWORD 'a12345678' CREATEDB;
+CREATE USER lessons WITH PASSWORD 'a12345678' CREATEDB;
+CREATE USER professors WITH PASSWORD 'a12345678' CREATEDB;
+CREATE USER reservations WITH PASSWORD 'a12345678' CREATEDB;
+CREATE USER resources WITH PASSWORD 'a12345678' CREATEDB;
+CREATE USER rooms WITH PASSWORD 'a12345678' CREATEDB;
+CREATE USER students WITH PASSWORD 'a12345678' CREATEDB;
+
+-- =====================================================
+-- FASE 2: Criar bancos de dados como postgres
+-- =====================================================
+
+-- Criar todos os bancos de dados como usuário postgres
+CREATE DATABASE sonar TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+CREATE DATABASE classes TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+CREATE DATABASE courses TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+CREATE DATABASE lessons TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+CREATE DATABASE professors TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+CREATE DATABASE reservations TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+CREATE DATABASE resources TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+CREATE DATABASE rooms TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+CREATE DATABASE students TEMPLATE template0 ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' TABLESPACE pg_default CONNECTION LIMIT -1;
+
+-- =====================================================
+-- FASE 3: Conceder privilégios aos usuários em seus respectivos bancos
+-- =====================================================
+
+-- Conceder privilégios completos aos usuários em seus respectivos bancos
 GRANT ALL PRIVILEGES ON DATABASE sonar TO sonar;
 GRANT ALL PRIVILEGES ON DATABASE classes TO classes;
 GRANT ALL PRIVILEGES ON DATABASE courses TO courses;
@@ -31,13 +49,88 @@ GRANT ALL PRIVILEGES ON DATABASE resources TO resources;
 GRANT ALL PRIVILEGES ON DATABASE rooms TO rooms;
 GRANT ALL PRIVILEGES ON DATABASE students TO students;
 
--- Conceder privilégios adicionais necessários
-ALTER USER sonar CREATEDB;
-ALTER USER classes CREATEDB;
-ALTER USER courses CREATEDB;
-ALTER USER lessons CREATEDB;
-ALTER USER professors CREATEDB;
-ALTER USER reservations CREATEDB;
-ALTER USER resources CREATEDB;
-ALTER USER rooms CREATEDB;
-ALTER USER students CREATEDB;
+-- =====================================================
+-- FASE 4: Configurar privilégios do schema public para cada banco
+-- =====================================================
+
+-- Configurar privilégios para o banco classes
+\c classes;
+GRANT ALL ON SCHEMA public TO classes;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO classes;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO classes;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO classes;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO classes;
+
+-- Configurar privilégios para o banco courses
+\c courses;
+GRANT ALL ON SCHEMA public TO courses;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO courses;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO courses;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO courses;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO courses;
+
+-- Configurar privilégios para o banco lessons
+\c lessons;
+GRANT ALL ON SCHEMA public TO lessons;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO lessons;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO lessons;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO lessons;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO lessons;
+
+-- Configurar privilégios para o banco professors
+\c professors;
+GRANT ALL ON SCHEMA public TO professors;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO professors;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO professors;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO professors;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO professors;
+
+-- Configurar privilégios para o banco reservations
+\c reservations;
+GRANT ALL ON SCHEMA public TO reservations;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO reservations;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO reservations;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO reservations;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO reservations;
+
+-- Configurar privilégios para o banco resources
+\c resources;
+GRANT ALL ON SCHEMA public TO resources;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO resources;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO resources;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO resources;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO resources;
+
+-- Configurar privilégios para o banco rooms
+\c rooms;
+GRANT ALL ON SCHEMA public TO rooms;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO rooms;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO rooms;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO rooms;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO rooms;
+
+-- Configurar privilégios para o banco students
+\c students;
+GRANT ALL ON SCHEMA public TO students;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO students;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO students;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO students;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO students;
+
+-- Configurar privilégios para o banco sonar
+\c sonar;
+GRANT ALL ON SCHEMA public TO sonar;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO sonar;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO sonar;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO sonar;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO sonar;
+
+-- =====================================================
+-- FASE 5: Verificação final
+-- =====================================================
+
+-- Listar todos os bancos de dados criados
+SELECT datname FROM pg_database WHERE datname IN ('sonar', 'classes', 'courses', 'lessons', 'professors', 'reservations', 'resources', 'rooms', 'students') ORDER BY datname;
+
+-- Listar todos os usuários criados
+SELECT usename FROM pg_user WHERE usename IN ('sonar', 'classes', 'courses', 'lessons', 'professors', 'reservations', 'resources', 'rooms', 'students') ORDER BY usename;
